46178046826ad02f84c26bad4bc35f56b4a8ec34
Makar Shatilov <m.point2011@yandex.ru>
2020-10-15 19:31:31 +0500

    added check linear independence

--- Program.cs
+++ Program.cs
@@ -5,130 +5,109 @@ namespace ConsoleApp2
 {
     class Program
     {
-        const double eps = 0.01;
-        const double dx = 10e-8;
-        static bool isTrigonomerical;
-        static double[] coefficient;
-        static double[] edges;
+        const int matrixSize = 4;
 
         static void Main(string[] args)
         {
-            СhooseTypeEquation();
-            ChooseParameters();
-            var f = isTrigonomerical ? new Func<double, double>((x) => coefficient[0] * Math.Cos(x) + coefficient[1] * x + coefficient[2])
-                : new Func<double, double>((x) => coefficient[0] * Math.Pow(x, 3) + coefficient[1] * x + coefficient[2]);
-            var res1 = HalveMethod(f, edges[0], edges[1]);
-            var res2 = NewtonMethod(f, edges[0], edges[1]);
-            var r1 = double.IsNaN(res1) ? "В заданном диапозоне корней нет" : "Результат, полученный методом деления отрезка: " + res1;
-            Console.WriteLine(r1);
-            var r2 = double.IsNaN(res2) ? "В заданном диапозоне корней нет" : "Результат, полученный методом Ньютона: " + res2;
-            Console.WriteLine(r2);
+            var matrix = ChooseParameters();
+            if (matrix == null)
+            {
+                Console.WriteLine("Система имеет множество решений");
+                Console.ReadKey();
+                return;
+            }
+            ShowMatrix(GetSimplifyMatrix(matrix));
+            ShowRoots(GetSolve(matrix));
+            
             Console.ReadKey();
         }
 
-        static double HalveMethod(Func<double, double> f, double e1, double e2)
+        private static double[,] GetSimplifyMatrix(double[,] matrix)
         {
-            double x;
-            var y = double.MaxValue;
-            while(true)
+            var h = 0;
+            while (h < matrixSize - 1)
             {
-                x = (e1 + e2) / 2;
-                y = Math.Abs(f(x));
-                if (Math.Abs(e2 - e1) < eps) 
-                    return x;
-                var halfEdge = GetHalveEdge(f, e1, e2, x);
-                if (halfEdge == null)
-                    return double.NaN;
-                e1 = halfEdge[0];
-                e2 = halfEdge[1];
+                SimplifyLine(matrix, h, h);
+                for (var l = h + 1; l < matrixSize; l++)
+                {
+                    if (matrix[h, h] == 0)
+                        SwapLines(matrix, h, h + 1);
+                    if (matrix[l, h] == 0)
+                        continue;
+                    var k = matrix[l, h];
+                    for (var i = h; i < matrixSize + 1; i++)
+                        matrix[l, i] = matrix[l, i] - k * matrix[h, i];
+                }
+                h++;
+            }
+            return matrix;
+        }
+
+
+        private static double[] GetSolve(double[,] matrix)
+        {
+            var roots = new double[matrixSize];
+            for (var i = matrixSize - 1; i >= 0; i--)
+            {
+                var p = 0.0;
+                for (var t = 0; t < matrixSize; t++)
+                    p += matrix[i, t] * roots[t];
+                roots[i] = (matrix[i, matrixSize] - p) / matrix[i, i];
+            }
+            return roots;
+        }
+
+        private static void SimplifyLine(double[,] matrix, int i1, int j1)
+        {
+            if (matrix[i1, j1] != 1)
+            {
+                var k = matrix[i1, j1];
+                for (var j = j1; j < 5; j++)
+                    matrix[i1, j] /= k;
             }
         }
 
-        static double[] GetHalveEdge(Func<double, double> f, double i1, double i2, double x0)
+        static void SwapLines(double[,] sys, int l1, int l2)
         {
-            var d1 = f(i1);
-            var d2 = f(x0);
-            if (d1 * d2 < 0)
-                return new double[2] { i1, x0 };
-            d1 = d2;
-            d2 = f(i2);
-            if (d1 * d2 < 0)
-                return new double[2] { x0, i2 };
+            var temp = 0.0;
+            for (var i = 0; i < matrixSize + 1; i++)
+            {
+                temp = sys[l1, i];
+                sys[l1, i] = sys[l2, i];
+                sys[l2, i] = temp;
+            }
+        }
+
+
+        private static double[,] ChooseParameters()
+        {
+            var parameters = new double[matrixSize, matrixSize + 1];
+            Console.WriteLine("Введите по порядку коэффициенты уравнений");
+            for (var i = 0; i < matrixSize; i++)
+            {
+                while(true)
+                {
+                    var m = GetUserInput();
+                    if (m.Length == matrixSize + 1)
+                    {
+                        for (var j = 0; j < matrixSize + 1; j++)
+                            parameters[i, j] = m[j];
+                        break;
+                    }
+                    ShowError();
+                }
+
+            }
+            if (IsLinearIndependence(parameters))
+                return parameters;
             return null;
         }
-       
-       static double NewtonMethod(Func<double, double> f, double e1, double e2)
-       {
-            double x0;
-            Func<double, double> secondDir; 
-            if (isTrigonomerical)
-                 secondDir = new Func<double, double>((x)=> -coefficient[0]*Math.Sin(x) + coefficient[1]);
-            else 
-                secondDir = new Func<double, double>((x) => 3 * coefficient[0] * Math.Pow(x, 2) + coefficient[1]);
-            if (f(e1) * secondDir(e1) > 0)
-                x0 = e1;
-            if (f(e2) * secondDir(e2) > 0)
-                x0 = e2;
-            else
-                return double.NaN;
-            while (Math.Abs(f(x0)) >= eps)
-            {
-                var d = (f(x0 + dx) - f(x0)) / dx;
-                x0 = x0 - (f(x0) / d);
-            }
-            return x0;
-        }
 
-        private static void ChooseParameters()
-        {
-            while (true)
-            {
-                Console.WriteLine("Введите по порядку коэффициенты a, b и с");
-                coefficient = GetUserInput();
-                if (coefficient.Length == 3)
-                {
-                    bool flag = false;
-                    while (true)
-                    {
-                        Console.WriteLine("Введите диапозон нахождения корня");
-                        edges = GetUserInput();
-                        if (edges.Length == 2)
-                        {
-                            flag = true;
-                            break;
-                        }
-                        ShowError();
-                    }
-                    if (flag)
-                        break;
-                }
-                ShowError();
-            }
-        }
 
-        private static void СhooseTypeEquation()
-        {
-            while (true)
-            {
-                Console.WriteLine("Выберите 0 для выбора уровнения вида \n   acos(x) + bx + c = 0 \nили 1 для уравнения вида \n    ax^3 + bx + c = 0");
-                var chose = GetUserInput();
-                if (chose.Length > 0 && chose[0] == 0)
-                {
-                    isTrigonomerical = true;
-                    break;
-                }
-                if (chose.Length > 0 && chose[0] == 1)
-                {
-                    isTrigonomerical = false;
-                    break;
-                }
-                ShowError();
-            }
-        }
         static double[] GetUserInput()
         {
             return Console.ReadLine().Split(' ')
-                           .Where(x => !string.IsNullOrWhiteSpace(x) && double.TryParse(x, out var d) )
+                           .Where(x => !string.IsNullOrWhiteSpace(x) && double.TryParse(x, out var d))
                            .Select(x => double.Parse(x))
                            .ToArray();
         }
@@ -136,6 +115,70 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Ошибка! Следуйте командам");
             Console.WriteLine();
+        }
+
+        static void ShowMatrix(double[,] matrix)
+        {
+            Console.WriteLine();
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == matrix.GetLength(1) - 1)
+                        Console.Write(" | ");
+                    if (matrix[i, j] >= 10 || matrix[i, j] < 0)
+                        Console.Write(matrix[i, j] + "  ");
+                    else if (matrix[i, j] != Math.Truncate(matrix[i, j]))
+                        Console.Write(matrix[i, j] + " ");
+                    else
+                        Console.Write(matrix[i, j] + "   ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+
+        static void ShowRoots(double[] roots)
+        {
+            for (var i = 0; i < roots.Length; i++)
+            {
+                Console.WriteLine($"x{i} =  {roots[i]}");
+            }
+        }
+
+        static bool IsLinearIndependence(double[,] matrix)
+        {
+            for (var i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (var j = i + 1; j < matrix.GetLength(0); j++)
+                {
+                    var count = 0;
+                    var pivot = 0.0;
+                    if (matrix[j, 0] == 0)
+                    {
+                        if (matrix[i, 0] == 0)
+                            count++;
+                        else continue;
+                    }
+                    else pivot = matrix[i, 0] / matrix[j, 0];
+                    for (var k = 1; k < matrix.GetLength(1) - 1; k++)
+                    {
+                        if (matrix[j, k] == 0)
+                        {
+                            if (matrix[i, k] == 0)
+                                count++;
+                            continue;
+                        }
+                        if (matrix[i, k] / matrix[j, k] == pivot)
+                            count++;
+                    }
+                    if (count == matrix.GetLength(0) - 1)
+                        return false;
+                }
+            }
+            return true;
+
         }
     }
 }
